0e5bcad74ddbc5a45f5cba9fd444e3e2027f8f79
Bukhari_Inst <bukhari414647@gmail.com>
2022-12-07 11:44:27 +0700

    landing page - hospot banner

---
+++ app/Views/parts/hotspot_banner.php
@@ -0,0 +1,88 @@
+<section class="my-10 position-relative">
+    <!-- SVG Shape Divider-->
+    <div class="position-absolute z-index-50 text-white top-0 start-0 end-0">
+        <svg class="align-self-start d-flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1283 53.25">
+            <polygon fill="currentColor" points="1283 0 0 0 0 53.25 1283 0" />
+        </svg>
+    </div>
+    <!-- /SVG Shape Divider-->
+
+    <div class="w-100 h-100 bg-img-cover bg-pos-center-center hotspot-container py-5 py-md-7 py-lg-10" style="background-image: url(<?= base_url() ?>/assets/images/banners/banner-5.jpg);">
+        <div class="hotspot d-none d-lg-block" data-options='{
+                    "placement": {
+                        "left": "68%",
+                        "bottom": "40%"
+                    },
+                    "alwaysVisible": true,
+                    "alwaysAnimate": true,
+                    "contentTarget": "#hotspot-one",
+                    "trigger": "mouseenter"
+                }'>
+        </div>
+        <div class="hotspot d-none d-lg-block" data-options='{
+                    "placement": {
+                        "left": "53%",
+                        "top": "40%"
+                    },
+                    "alwaysVisible": true,
+                    "alwaysAnimate": true,
+                    "contentTarget": "#hotspot-one"
+                }'>
+        </div>
+        <div class="container py-lg-8 position-relative z-index-10 d-flex align-items-center" data-aos="fade-left">
+            <div class="py-8 d-flex justify-content-end align-items-start align-items-lg-end flex-column col-lg-4 text-lg-end">
+                <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Kualitas Terbaik</p>
+                <h2 class="display-5 fw-bold mb-3">Nyaman Dipakai</h2>
+                <p class="lead d-none d-lg-block">Mencari abaya? Anda di tempat yang tepat! Koleksi kami
+                    menyediakan bermacam-macam desain modis dan fashion tinggi. Anda akan menemukan abaya sempurna
+                    yang Anda sukai. Tersedia dalam berbagai
+                    panjang dan ukuran, penuh dengan desain elegan. Kami akan membuat Anda menarik di
+                    setiap kesempatan.</p>
+                <a class="text-decoration-none fw-bolder" href="#">Berbelanja sekarang &rarr;</a>
+            </div>
+        </div>
+
+        <!-- Example Hotspot HTML Content -->
+        <div id="hotspot-one" class="d-none">
+            <div class="m-n2 rounded overflow-hidden">
+                <div class="mb-1 bg-light d-flex justify-content-center">
+                    <div class="f-w-48 px-3 pt-3">
+                        <img class="img-fluid" src="<?= base_url(); ?>/assets/images/products/product-3.jpg" alt="img">
+                    </div>
+                </div>
+                <div class="px-4 py-4 text-center">
+                    <div class="d-flex justify-content-center align-items-center mx-auto mb-1">
+                        <!-- Review Stars Small-->
+                        <div class="rating position-relative d-table">
+                            <div class="position-absolute stars" style="width: 80%">
+                                <i class="ri-star-fill text-dark mr-1"></i>
+                                <i class="ri-star-fill text-dark mr-1"></i>
+                                <i class="ri-star-fill text-dark mr-1"></i>
+                                <i class="ri-star-fill text-dark mr-1"></i>
+                                <i class="ri-star-fill text-dark mr-1"></i>
+                            </div>
+                            <div class="stars">
+                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
+                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
+                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
+                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
+                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
+                            </div>
+                        </div> <span class="small fw-bolder ms-2 text-muted"> 4.4 (1289)</span>
+                    </div>
+                    <p class="mb-0 mx-4">New Arrival - Catleya Dress</p>
+                    <p class="lead lh-1 m-0 fw-bolder mt-2 mb-3">Rp750.000</p>
+                    <a href="./product.html" class="fw-bolder text-link-border pb-1 fs-6">Dress details <i class="ri-arrow-right-line align-bottom"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SVG Shape Divider-->
+    <div class="position-absolute z-index-50 text-white bottom-0 start-0 end-0">
+        <svg class="align-self-end d-flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1283 53.25">
+            <polygon fill="currentColor" points="0 53.25 1283 53.25 1283 0 0 53.25" />
+        </svg>
+    </div>
+    <!-- /SVG Shape Divider-->
+</section>
