49a2c3eb7b943b439d9ebf90462c4abe4ad6057f
Bukhari_Inst <bukhari414647@gmail.com>
2022-12-07 11:43:17 +0700

    landing page - product carrousel

--- app/Views/pages/landing_page.php
+++ app/Views/pages/landing_page.php
@@ -22,48 +22,42 @@
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop Kathmandu">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-1.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-1.svg" alt="img">
                         </a>
                     </div>
                     <div class="me-2 f-w-20 m-4 ms-md-0 mt-md-0 mb-md-0">
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop Billabong">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-2.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-2.svg" alt="img">
                         </a>
                     </div>
                     <div class="me-2 f-w-20 m-4 ms-md-0 mt-md-0 mb-md-0">
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop Oakley">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-9.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-9.svg" alt="img">
                         </a>
                     </div>
                     <div class="me-2 f-w-20 m-4 ms-md-0 mt-md-0 mb-md-0">
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop Patagonia">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-4.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-4.svg" alt="img">
                         </a>
                     </div>
                     <div class="me-2 f-w-20 m-4 ms-md-0 mt-md-0 mb-md-0">
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop North Face">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-5.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-5.svg" alt="img">
                         </a>
                     </div>
                     <div class="me-2 f-w-20 m-4 ms-md-0 mt-md-0 mb-md-0">
                         <a class="d-block" href="./category.html" data-bs-toggle="tooltip" data-bs-placement="top"
                             title="Shop Salomon">
                             <img class="img-fluid d-table mx-auto"
-                                src="<?= base_url(); ?>/assets/images/logos/logo-7.svg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
+                                src="<?= base_url(); ?>/assets/images/logos/logo-7.svg" alt="img">
                         </a>
                     </div>
                     <a href="./category.html" class="btn btn-link fw-bolder">Explore All Brands <i
@@ -503,7 +497,7 @@
                             class="d-flex text-decoration-none flex-column justify-content-center align-items-center">
                             <span class="btn btn-dark btn-icon mb-3"><i
                                     class="ri-arrow-right-line ri-lg lh-1"></i></span>
-                            <span class="lead fw-bolder">See All</span>
+                            <span class="lead fw-bolder">Lihat Semua</span>
                         </a>
                     </div>
                 </div>
@@ -528,101 +522,12 @@
     <!-- / Staff Picks-->
 
     <!-- Image Hotspot Banner-->
-    <section class="my-10 position-relative">
-        <!-- SVG Shape Divider-->
-        <div class="position-absolute z-index-50 text-white top-0 start-0 end-0">
-            <svg class="align-self-start d-flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1283 53.25">
-                <polygon fill="currentColor" points="1283 0 0 0 0 53.25 1283 0" />
-            </svg>
-        </div>
-        <!-- /SVG Shape Divider-->
-
-        <div class="w-100 h-100 bg-img-cover bg-pos-center-center hotspot-container py-5 py-md-7 py-lg-10"
-            style="background-image: url(<?= base_url() ?>/assets/images/banners/banner-5.jpg);">
-            <div class="hotspot d-none d-lg-block" data-options='{
-                    "placement": {
-                        "left": "68%",
-                        "bottom": "40%"
-                    },
-                    "alwaysVisible": true,
-                    "alwaysAnimate": true,
-                    "contentTarget": "#hotspot-one",
-                    "trigger": "mouseenter"
-                }'>
-            </div>
-            <div class="hotspot d-none d-lg-block" data-options='{
-                    "placement": {
-                        "left": "53%",
-                        "top": "40%"
-                    },
-                    "alwaysVisible": true,
-                    "alwaysAnimate": true,
-                    "contentTarget": "#hotspot-one"
-                }'>
-            </div>
-            <div class="container py-lg-8 position-relative z-index-10 d-flex align-items-center" data-aos="fade-left">
-                <div
-                    class="py-8 d-flex justify-content-end align-items-start align-items-lg-end flex-column col-lg-4 text-lg-end">
-                    <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Kualitas Terbaik</p>
-                    <h2 class="display-5 fw-bold mb-3">Nyaman Dipakai</h2>
-                    <p class="lead d-none d-lg-block">Mencari abaya? Anda di tempat yang tepat! Koleksi kami
-                        menyediakan bermacam-macam desain modis dan fashion tinggi. Anda akan menemukan abaya sempurna
-                        yang Anda sukai. Tersedia dalam berbagai
-                        panjang dan ukuran, penuh dengan desain elegan. Kami akan membuat Anda menarik di
-                        setiap kesempatan.</p>
-                    <a class="text-decoration-none fw-bolder" href="#">Berbelanja sekarang &rarr;</a>
-                </div>
-            </div>
-
-            <!-- Example Hotspot HTML Content -->
-            <div id="hotspot-one" class="d-none">
-                <div class="m-n2 rounded overflow-hidden">
-                    <div class="mb-1 bg-light d-flex justify-content-center">
-                        <div class="f-w-48 px-3 pt-3">
-                            <img class="img-fluid" src="<?= base_url(); ?>/assets/images/products/product-3.jpg"
-                                alt="Bootstrap 5 Template by Pixel Rocket">
-                        </div>
-                    </div>
-                    <div class="px-4 py-4 text-center">
-                        <div class="d-flex justify-content-center align-items-center mx-auto mb-1">
-                            <!-- Review Stars Small-->
-                            <div class="rating position-relative d-table">
-                                <div class="position-absolute stars" style="width: 80%">
-                                    <i class="ri-star-fill text-dark mr-1"></i>
-                                    <i class="ri-star-fill text-dark mr-1"></i>
-                                    <i class="ri-star-fill text-dark mr-1"></i>
-                                    <i class="ri-star-fill text-dark mr-1"></i>
-                                    <i class="ri-star-fill text-dark mr-1"></i>
-                                </div>
-                                <div class="stars">
-                                    <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                    <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                    <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                    <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                    <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                </div>
-                            </div> <span class="small fw-bolder ms-2 text-muted"> 4.4 (1289)</span>
-                        </div>
-                        <p class="mb-0 mx-4">New Arrival - Catleya Dress</p>
-                        <p class="lead lh-1 m-0 fw-bolder mt-2 mb-3">Rp750.000</p>
-                        <a href="./product.html" class="fw-bolder text-link-border pb-1 fs-6">Dress details <i
-                                class="ri-arrow-right-line align-bottom"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- SVG Shape Divider-->
-        <div class="position-absolute z-index-50 text-white bottom-0 start-0 end-0">
-            <svg class="align-self-end d-flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1283 53.25">
-                <polygon fill="currentColor" points="0 53.25 1283 53.25 1283 0 0 53.25" />
-            </svg>
-        </div>
-        <!-- /SVG Shape Divider-->
-    </section>
+    <?= $this->include('parts/hotspot_banner'); ?>
     <!-- / Image Hotspot Banner-->
 
     <!-- Linked Product Carousels-->
+    <?= $this->include('parts/product_carousels'); ?>
+    <!-- / Linked Product Carousels-->
 
 </section>
 <!-- / Main Section-->
