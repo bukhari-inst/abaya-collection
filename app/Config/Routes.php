98ca220157167b9521cd733dc23e650f20206703
Bukhari_Inst <bukhari414647@gmail.com>
2022-12-09 09:44:06 +0700

    admin page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/category', 'Home::category');
+$routes->get('/admin', 'Admin::index');
 
 /*
  * --------------------------------------------------------------------
