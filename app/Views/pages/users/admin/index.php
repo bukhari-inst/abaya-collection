cb67e31609f9e785f127ec74baee881599636599
Bukhari_Inst <bukhari414647@gmail.com>
2022-12-09 09:44:17 +0700

    admin page

---
+++ app/Views/pages/users/admin/index.php
@@ -0,0 +1,57 @@
+<?= $this->include('parts/admin/head'); ?>
+
+<!-- ======= Header ======= -->
+<?= $this->include('parts/admin/header'); ?>
+
+<!-- ======= Sidebar ======= -->
+<?= $this->include('parts/admin/sidebar'); ?>
+
+<main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Dasboard</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item">Pages</li>
+                <li class="breadcrumb-item active">Blank</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Produk</h5>
+                        <table class="table datatable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">Age</th>
+                                    <th scope="col">Start Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<?= $this->include('parts/admin/footer'); ?>
